628e8f040807ea1427990262ffa987ecbe26e6b8
usman-tahir-arhamsoft <usman.tahir@arhamsoft.org>
2023-07-25 16:55:01 +0500

    ip issue fixed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '192.168.27.10', user: 'arhamsoft', roles: %w{app db web}
+server '202.166.170.106', user: 'arhamsoft', roles: %w{app db web}
 
 
 
